681760f8be2798f6ee3dcbe9c9776b93da2a4148
Loskir <kl@sinker.ru>
2020-07-10 18:46:44 +0300

    Add /queue

--- middlewares/start.js
+++ middlewares/start.js
@@ -1,4 +1,4 @@
-const {Composer} = require('telegraf')
+const {Composer, Extra} = require('telegraf')
 
 const Results = require('../models/results')
 
@@ -17,6 +17,13 @@ Inspired by openprocessing.org/sketch/624879
     disable_web_page_preview: true,
   })
 })
+
+const pluralizeIndex = (n) => {
+  if (n % 10 === 1 && n % 100 !== 11) {
+    return 0
+  }
+  return n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2
+}
 
 composer.on('photo', async (ctx) => {
   const url = await ctx.telegram.getFileLink(ctx.message.photo[ctx.message.photo.length - 1].file_id)
@@ -38,18 +45,32 @@ composer.on('photo', async (ctx) => {
     return ctx.reply(`Отлично, я начал рисовать твою картинку, подожди минутку`)
   }
 
-  const pluralizeIndex = (n) => {
-    if (n % 10 === 1 && n % 100 !== 11) {
-      return 0
-    }
-    return n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2
-  }
-
   const word = ['картинка', 'картинки', 'картинок'][pluralizeIndex(queue)]
 
   return ctx.reply(`Отлично, я положил твою картинку в очередь. Перед тобой ${queue} ${word}, подожди немного.`)
 
   // return processImage(url, ctx.chat.id)
+})
+
+composer.command('queue', async (ctx) => {
+  const queue = [
+    ...await Results.find({status: 0}).sort({created_at: 1}),
+  ]
+  let indexes = []
+  queue.forEach((result, i) => {
+    if (result.user_id === ctx.from.id) {
+      indexes.push(i)
+    }
+  })
+
+  const word = ['картинка', 'картинки', 'картинок'][pluralizeIndex(queue.length)]
+
+  return ctx.reply(
+    `Всего в очереди ${queue.length} ${word}
+
+Твои заявки: ${indexes.length > 0 ? indexes.map((v) => v + 1).map((v) => `${v}-я`).join(', ') : 'пусто'}`,
+    Extra.HTML()
+  )
 })
 
 module.exports = composer
